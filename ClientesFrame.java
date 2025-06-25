d9a4d9945445a480dc314add67d99466d83e804b
Beatriz <168593598+monossacaridea@users.noreply.github.com>
2025-06-25 15:00:16 -0300

    Update ClientesFrame.java

--- ClientesFrame.java
+++ ClientesFrame.java
@@ -308,67 +308,70 @@ public class ClientesFrame extends JFrame {
             new EmptyBorder(5, 15, 5, 15)
         ));
         
-        // Botão para salvar e validações
-        btnSalvarDialogo.addActionListener(e -> {
-        	  String nome = txtNome.getText().trim();
-              String telefone = txtTelefone.getText().trim();
-              String email = txtEmail.getText().trim();
-              String documento = txtDocumento.getText().trim();
-              StringBuilder erros = new StringBuilder();
-              
-              if (nome.isEmpty() || telefone.isEmpty() || email.isEmpty() || documento.isEmpty()) {
-                  JOptionPane.showMessageDialog(dialog, "Todos os campos são obrigatórios.", "Erro de Validação", JOptionPane.ERROR_MESSAGE);
-                  return;
-              }
-              
-              // Validação nome
-              if (!nome.matches("[a-zA-Z ]+")) {
-                  erros.append("- Use apenas letras no nome\n");
-              }
-              
-           // Valida telefone (permite números, espaços, parênteses e hífens)
-              if (!telefone.matches("^[\\d\\s()\\-]+$")) {
-                  JOptionPane.showMessageDialog(dialog, 
-                      "Telefone deve conter apenas números, espaços, parênteses ou hífens.", 
-                      "Erro de Validação", JOptionPane.ERROR_MESSAGE);
-                  return;
-              }
-
-              
-              // Validação email
-              if (!email.isEmpty()) {
-                  String[] partes = email.split("@");
-                  if (partes.length != 2 || 
-                      partes[0].isEmpty() || 
-                      !partes[1].contains(".") ||
-                      partes[1].indexOf(".") == 0 ||
-                      partes[1].endsWith(".")) {
-                      erros.append("- Email inválido. Use o formato: nome@dominio.com\n");
+        
+            btnSalvarDialogo.addActionListener(e -> {
+            	  String nome = txtNome.getText().trim();
+                  String telefone = txtTelefone.getText().trim();
+                  String email = txtEmail.getText().trim();
+                  String documento = txtDocumento.getText().trim();
+                  StringBuilder erros = new StringBuilder();
+                  
+                  if (nome.isEmpty() || telefone.isEmpty() || email.isEmpty() || documento.isEmpty()) {
+                      JOptionPane.showMessageDialog(dialog, "Todos os campos são obrigatórios.", "Erro de Validação", JOptionPane.ERROR_MESSAGE);
+                      return;
                   }
-              }
-              
-              // Validação documentos
-              String tipo = (String) cbTipo.getSelectedItem();
-              if ("NACIONAL".equals(tipo) && !documento.matches("\\d{11}")) {
-                  JOptionPane.showMessageDialog(dialog, "CPF deve conter 11 dígitos.", "Erro de Validação", JOptionPane.ERROR_MESSAGE);
-                  return;
-              }
-              
-              if (isEdit) {
-                  cliente.setNome(nome);
-                  cliente.setTelefone(telefone);
-                  cliente.setEmail(email);
-                  cliente.setDocumento(documento);
-                  controller.atualizarCliente(cliente);
-              } else {
-                  Cliente novoCliente = new Cliente(0, nome, telefone, email, tipo, documento);
-                  controller.salvarCliente(novoCliente);
-              }
-
-              atualizarTabela();
-              dialog.dispose();
-        });
-
+                  
+                  //validação nome
+                  if (!nome.matches("[a-zA-Z ]+")) {
+                      erros.append("- Use apenas letras no nome\n");
+                  }
+                  
+                  //validação telefone
+                  if (!telefone.matches("^[\\d\\s()\\-]+$")) {
+                      JOptionPane.showMessageDialog(dialog, 
+                          "Telefone deve conter apenas números, espaços, parênteses ou hífens.", 
+                          "Erro de Validação", JOptionPane.ERROR_MESSAGE);
+                      return;
+                  }
+                  
+                //validação email
+                  if (!email.isEmpty()) {
+                      String[] partes = email.split("@");
+                      if (partes.length != 2 || 
+                          partes[0].isEmpty() || 
+                          !partes[1].contains(".") ||
+                          partes[1].indexOf(".") == 0 ||
+                          partes[1].endsWith(".")) {
+                          erros.append("- Email inválido. Use o formato: nome@dominio.com\n");
+                      JOptionPane.showMessageDialog(dialog, "Email inválido.", "Use o formato: nome@dominio.com", JOptionPane.ERROR_MESSAGE);
+                      return;
+                      }
+                  }
+                  
+                  //validação documentos
+                  String tipo = (String) cbTipo.getSelectedItem();
+                  if ("NACIONAL".equals(tipo) && !documento.matches("\\d{11}")) {
+                      JOptionPane.showMessageDialog(dialog, "CPF deve conter 11 dígitos.", "Erro de Validação", JOptionPane.ERROR_MESSAGE);
+                      return;
+                  }
+                  
+                  //salvar apos validação
+                  if (isEdit) {
+                      cliente.setNome(nome);
+                      cliente.setTelefone(telefone);
+                      cliente.setEmail(email);
+                      cliente.setDocumento(documento);
+                      controller.atualizarCliente(cliente);
+                  } else {
+                      Cliente novoCliente = new Cliente(0, nome, telefone, email, tipo, documento);
+                      controller.salvarCliente(novoCliente);
+                  }
+                  
+                  atualizarTabela();
+                  dialog.dispose();
+                  
+            });
+            
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 10, 10));
         buttonPanel.setBackground(Color.WHITE);
         buttonPanel.add(btnSalvarDialogo);
